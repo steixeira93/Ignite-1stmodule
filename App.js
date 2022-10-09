b9110b06e5bc77dc5616899be95f567fca6dd02b
Sunshine <steixeira1337@gmail.com>
2022-10-08 21:05:39 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> React Native</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  }
+});
